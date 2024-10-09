f300d9ee898d9f658fe81c45f0281067068faf02
Arun Vishwakarma <167244423+ArunVishwakarmaAV@users.noreply.github.com>
2024-10-09 19:11:52 +0530

    Time: 1 ms (58.43%), Space: 41.4 MB (64.62%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int count = 0;
+        
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    count++;
+                }
+            }
+        }
+        
+        return count + stack.size();
+    }
+}
