1fed4743546b9b52ebbeedaa992aa91f884f51cc
Arun Vishwakarma <167244423+ArunVishwakarmaAV@users.noreply.github.com>
2024-11-10 22:25:56 +0530

    Time: 1 ms (22.54%), Space: 28.3 MB (16.31%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,8 +1,10 @@
+import numpy as np
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n<0:
-            return False
-        else:
-            n=bin(n)
-            return n[2:].count("1") ==1
+        i = 0
+        while i < 32:
+            if 2**i == n:
+                return True
+            i += 1
+        return False
         
